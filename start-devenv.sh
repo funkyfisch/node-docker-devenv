1e087acd7323df09bdbe47b6ece936d7febb1b6d
funkyfisch <funkyfisch@gmail.com>
2021-03-02 13:31:52 +0100

    Add .dockerignore, fix image name, use full arg names
      - add .dockerignore for consistency
    
      - change script to use full argument names instead
        of shorthand notations for better readability
        and a learning experience
    
      - fix image name reference in script

--- start-devenv.sh
+++ start-devenv.sh
@@ -7,17 +7,18 @@ DEV_PORT=${DEV_PORT:-4000}
 HOT_RELOAD_PORT=${HOT_RELOAD_PORT:-35729}
 
 docker run \
+    --interactive \
+    --env USER=${USER} \
+    --hostname node-docker-devenv \
+    --publish ${DEV_PORT}:8080 \
+    --publish ${HOT_RELOAD_PORT}:${HOT_RELOAD_PORT} \
     --rm \
-    -it \
-    -e USER=${USER} \
-    -h node-docker-devenv \
-    -p ${DEV_PORT}:8080 \
-    -p ${HOT_RELOAD_PORT}:${HOT_RELOAD_PORT} \
-    -v "$(pwd)":/mnt/workspace \
-    -v /etc/passwd:/etc/passwd:ro \
-    -v /etc/group:/etc/group:ro \
-    -v /etc/timezone:/etc/timezone:ro \
-    -v ${HOME}:/home/${USER}/ \
-    -w /mnt/workspace \
-    nodejs-docker-devenv:latest \
+    --tty \
+    --volume "$(pwd)":/mnt/workspace \
+    --volume /etc/passwd:/etc/passwd:ro \
+    --volume /etc/group:/etc/group:ro \
+    --volume /etc/timezone:/etc/timezone:ro \
+    --volume ${HOME}:/home/${USER}/ \
+    --workdir /mnt/workspace \
+    node-docker-devenv:latest \
     bash
